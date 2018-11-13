a8823939c3c5255fd8b97a0ddde3b5f492561589
yuliya <yuliya.duniakk@gmail.com>
2018-11-13 20:05:26 +0200

    Trecia uzduotis

--- FifthLesson/FifthLesson/Program.cs
+++ FifthLesson/FifthLesson/Program.cs
@@ -14,9 +14,12 @@ namespace FifthLesson
             numbers[0] = Convert.ToInt32(Console.ReadLine());
             numbers[1] = Convert.ToInt32(Console.ReadLine());
             numbers[2] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Didziausias yra {numbers.Max()}");
+            Console.WriteLine($"Maximum is {numbers.Max()}");
             Boolean range = inRange(numbers[0], numbers[1], numbers[2]);
-            Console.WriteLine($" Is it in range? {range}");
+            Console.WriteLine($"Is it in range? {range}");
+            Boolean prime = isPrimal(numbers[0]);
+            Console.WriteLine($"Is it primal? {prime}");
+
             Console.ReadKey();
 
 
@@ -27,6 +30,16 @@ namespace FifthLesson
         {
 
             if ((num1>num2 && num1<num3) || (num1 < num2 && num1 > num3))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        static Boolean isPrimal(int num1)
+        {
+
+            if (num1%2!=0 && num1%3!=0 && num1 % 5 != 0 && num1 % 7 != 0)
             {
                 return true;
             }
