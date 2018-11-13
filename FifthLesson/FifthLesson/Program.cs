308b25bfb944dcf49d41f0cc30f563dd1932b616
yuliya <yuliya.duniakk@gmail.com>
2018-11-13 19:55:23 +0200

    antra uzduotis

--- FifthLesson/FifthLesson/Program.cs
+++ FifthLesson/FifthLesson/Program.cs
@@ -15,8 +15,22 @@ namespace FifthLesson
             numbers[1] = Convert.ToInt32(Console.ReadLine());
             numbers[2] = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Didziausias yra {numbers.Max()}");
+            Boolean range = inRange(numbers[0], numbers[1], numbers[2]);
+            Console.WriteLine($" Is it in range? {range}");
             Console.ReadKey();
+
+
             
+        }
+
+        static Boolean inRange(int num1, int num2, int num3)
+        {
+
+            if ((num1>num2 && num1<num3) || (num1 < num2 && num1 > num3))
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
